4b4a0a499cbae2bc2aeae288fffe852dbcc64c41
andaicheng <andaicheng@gmail.com>
2019-06-09 14:10:13 +0800

    encapsulation

---
+++ _encapsulation/encap_test.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+	"unsafe"
+)
+
+type Employee struct {
+	Id   int
+	Name string
+	Age  int
+}
+
+func (e Employee) string() string {
+	fmt.Printf("address is %x \n", unsafe.Pointer(&e.Name))
+	return fmt.Sprintf("Id:%d-Name:%s-Age:%d", e.Id, e.Name, e.Age)
+}
+
+func (e *Employee) string2() string {
+	fmt.Printf("address is %x \n", unsafe.Pointer(&e.Name))
+	return fmt.Sprintf("Id:%d-Name:%s-Age:%d", e.Id, e.Name, e.Age)
+}
+
+func TestStructMethod(t *testing.T) {
+	e := &Employee{1, "adc", 18}
+	fmt.Printf("address is %x \n", unsafe.Pointer(&e.Name))
+	t.Log(e.string())
+	t.Log(e.string2())
+}
+
+func TestCreateObj(t *testing.T) {
+	e := Employee{1, "adc", 18}
+	e1 := Employee{Name: "zkx", Age: 19}
+	e2 := new(Employee) //返回指针
+	e2.Id = 3
+	e2.Age = 23
+	e2.Name = "sdd"
+
+	t.Log(e)
+	t.Log(e1)
+	t.Log(e2)
+
+	t.Logf("e is %T", e)
+	t.Logf("e2 is %T", e2)
+}
